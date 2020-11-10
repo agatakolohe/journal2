675b7d668352af84717500530bce6ce5a1fa82e6
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-11-09 16:13:00 -0800

    successfully tested letterCount method.
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- src/entry.js
+++ src/entry.js
@@ -2,3 +2,9 @@ function Entry(title, body) {
   this.title = title;
   this.body = body;
 }
+Entry.prototype.wordCount = function(body) {
+  return this.body.split(" ").length;
+}
+Entry.prototype.letterCount = function(body) {
+  return this.body.match(/[a-z]/gi).length;
+}
