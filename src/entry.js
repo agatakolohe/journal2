66de22926c32e6e935cd5e2d6fdaa496a0a7bf72
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-11-09 21:45:39 -0800

    Completes working environment for journal project
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- src/entry.js
+++ src/entry.js
@@ -1,13 +1,13 @@
-function Entry(title, body) {
+export default function Entry(title, body) {
   this.title = title;
   this.body = body;
 }
 Entry.prototype.wordCount = function() {
   return this.body.split(" ").length;
-}
+},
 Entry.prototype.letterCount = function() {
   return this.body.match(/[a-z]/gi).length;
-}
+};
 Entry.prototype.getTeaser = function() {
-   return this.body.split(" ").slice(0,8).join(" ") + "...";
-}
+  return this.body.split(" ").slice(0,8).join(" ") + "...";
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Entry from './entry.js';
+
 $(document).ready(function(){
   $('#entry').submit(function(event){
     event.preventDefault();
     const entryTitle = $('#entry-title').val();
     const entryBody = $('#entry-body').val();
-    const entry = new Entry(entryTitle, entryBody)
+    const entry = new Entry(entryTitle, entryBody);
     $('#published-entry').text(entry.getTeaser());
   });
 });
